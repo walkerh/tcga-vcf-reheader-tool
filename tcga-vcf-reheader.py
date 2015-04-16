8e4ecbc2b6f90f1b276d821fb50a342d05626113
Walker Hale IV <walker.hale.iv@gmail.com>
2015-04-16 10:24:08 -0400

    Extract function write_fixed_headers

--- tcga-vcf-reheader.py
+++ tcga-vcf-reheader.py
@@ -39,13 +39,17 @@ def run(args):
     asserted_headers = set(item[0] for item in fixed_headers if item[1])
     with open(args.input_file_path) as fin:
         with open(args.output_file_path, 'w') as fout:
-            for name, ignored, value in fixed_headers:
-                write_meta_line(fout, name, value)
+            write_fixed_headers(fout, fixed_headers)
             for id, params in args.parameter_map['samples'].items():
                 sample_line = SAMPLE_LINE_FORMAT.format(
                     id=id, **dict(params, **CONFIG['fixed_sample_params'])
                 )
                 write_stripped_line(fout, sample_line)
+
+
+def write_fixed_headers(fout, fixed_headers):
+    for name, ignored, value in fixed_headers:
+        write_meta_line(fout, name, value)
 
 
 def write_meta_line(fout, name, value):
